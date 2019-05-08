b7a0a641260f7518d843c1f58ebe76cc5701c616
David Thomas <david.thomas10111@yahoo.com>
2019-05-08 09:13:34 +0800

    created new ios project

---
+++ SWEN3000/Quick Vault/Quick Vault/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quick Vault
+//
+//  Created by administritor on 2019/5/8.
+//  Copyright © 2019 KOD Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

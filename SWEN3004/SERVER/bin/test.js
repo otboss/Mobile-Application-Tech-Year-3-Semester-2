472ed5f859a120849b712c5864548b005b6c2cf5
otboss <otsurfer6@gmail.com>
2019-05-16 12:07:57 -0500

    Included TypeScript into CipherChat Server

--- SWEN3004/SERVER/bin/test.js
+++
@@ -1,7 +0,0 @@
-define("server", ["require", "exports", "express"], function (require, exports, express) {
-    "use strict";
-    exports.__esModule = true;
-    //const express = require("express");
-    var f = express;
-    var t = false;
-});
